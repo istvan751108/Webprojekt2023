b7e2a082692468887e21ce8e1b378dfcdb4141c0
István Szabó <istvan751108@gmail.com>
2023-03-26 20:27:27 +0200

    Fix it.

--- src/main/java/hu/nye/webapp/movies/controller/MovieControllerAdvice.java
+++ src/main/java/hu/nye/webapp/movies/controller/MovieControllerAdvice.java
@@ -2,7 +2,7 @@ package hu.nye.webapp.movies.controller;
 
 import hu.nye.webapp.movies.exception.MovieNotFoundException;
 import hu.nye.webapp.movies.exception.MovieValidationException;
-import hu.nye.webapp.movies.reponse.ErrorResponse;
+import hu.nye.webapp.movies.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
--- src/main/java/hu/nye/webapp/movies/response/ErrorResponse.java
+++ src/main/java/hu/nye/webapp/movies/response/ErrorResponse.java
@@ -1,4 +1,4 @@
-package hu.nye.webapp.movies.reponse;
+package hu.nye.webapp.movies.response;
 
 import java.util.List;
 
